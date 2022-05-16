ac0b01c15ed455f4ba72291f55c813ed123ec312
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-05-16 17:56:53 -0300

    Initial Commit

---
+++ SwUi_Profile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwUi_Profile
+//
+//  Created by Marcelo Sampaio on 16/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwUi_Profile/SwUi_ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUi_ProfileApp.swift
+//  SwUi_Profile
+//
+//  Created by Marcelo Sampaio on 16/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwUi_ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
