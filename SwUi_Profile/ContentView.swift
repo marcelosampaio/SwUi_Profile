7b9111af790b540a3e1cb7c2e7a4ea147e7e63a4
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-05-16 20:03:51 -0300

    checkpoint

--- SwUi_Profile/ContentView.swift
+++ SwUi_Profile/ContentView.swift
@@ -7,10 +7,66 @@
 
 import SwiftUI
 
+struct User {
+    var picture: String
+    var name: String
+    var nick: String
+    var followers: Double
+}
+
+
 struct ContentView: View {
+    
+    var user = User(picture: "profilePicture",
+                    name: "Username",
+                    nick: "@username",
+                    followers: 1501)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image("profilePicture")
+                .resizable()
+                .frame(width: 180, height: 180)
+                .cornerRadius(90)
+            
+            Text("Username")
+                .font(.system(.title).bold())
+             
+            Text("@username")
+                .font(.system(size: 20, weight: .regular))
+                .foregroundColor(.gray)
+            
+            Text("1.500 followers")
+                .font(.system(size: 22, weight: .light))
+                .padding(.top, 40)
+            
+            VStack {
+                Button{} label: {
+                    Label("Follow", systemImage: "rectangle")
+                        .font(.title3)
+                        .frame(maxWidth: . infinity)
+                }
+                .buttonStyle(.borderedProminent)
+                .controlSize(.large)
+                
+        
+                Button{} label: {
+                    Label("Contact Us", systemImage: "mail")
+                        .font(.title3)
+                        .frame(maxWidth: . infinity)
+                }
+                .buttonStyle(.borderedProminent)
+                .controlSize(.large)
+                .disabled(true)
+                
+                
+            }
+            .padding(24)
+        
+            
+        }
+            .ignoresSafeArea()
+        
     }
 }
 
